e9541dcc4943d64b1664a22335ec17b7484733b7
ntuvera <ntuvera@gmail.com>
2014-09-18 16:36:17 -0400

    ajax on success replaces content with view count

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,11 +6,18 @@ $.ajax({
   }
 })
 */
+// https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key= AIzaSyCTycfGZRxyM6KUBkgcyuiC0Dvl1BTFUuI&part=snippet,contentDetails,statistics,status
 
-var test =
-$.ajax({
-  url: 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key= AIzaSyCTycfGZRxyM6KUBkgcyuiC0Dvl1BTFUuI&part=snippet,contentDetails,statistics,status',
-  success: function(data) {
-    console.log(data)
-  }
-})
+var youtubeRequest = function(){
+  var query = $('#target').val()
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/videos?id='+ query +'&key= AIzaSyCTycfGZRxyM6KUBkgcyuiC0Dvl1BTFUuI&part=snippet,contentDetails,statistics,status',
+    success: function(data) {
+      console.log(data);
+      object = data;
+      $('#content').empty();
+      $('#content').text(object.items[0].statistics.viewCount + ' views');
+      return data;
+    }
+  })
+}
